d5e505d8db5353a4e3033e789a1cf6f58b20d4e4
Stephanie Englberger <stephaniekoebich@aol.com>
2026-01-08 10:37:05 +0100

    Add registration view

---
+++ auth_app/api/views.py
@@ -0,0 +1,27 @@
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from .serializers import RegistrationSerializer, CustomTokenObtainPairSerializer
+
+
+class RegistrationView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        data={}
+        
+        if serializer.is_valid():
+            saved_account = serializer.save()
+            data = {
+                "username": saved_account.username,
+                "email": saved_account.email,
+                "user_id": saved_account.pk,
+            }
+            return Response(data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
