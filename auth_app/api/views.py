ea108292a1749f848776c2e7d6c7cbe0ff6e8b90
Stephanie Englberger <stephaniekoebich@aol.com>
2026-01-08 10:38:37 +0100

    Add views for auth with HTTPonly cookies

--- auth_app/api/views.py
+++ auth_app/api/views.py
@@ -25,3 +25,60 @@ class RegistrationView(APIView):
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         
+
+class CookieTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        refresh = serializer.validated_data.get('refresh')
+        access = serializer.validated_data.get('access')
+
+        response = Response(status=status.HTTP_200_OK)
+        response.set_cookie(
+            key='refresh_token',
+            value=str(refresh),
+            httponly=True,
+            secure=True,
+            samesite='Lax',
+        )
+
+        response.set_cookie(
+            key='access_token',
+            value=str(access),
+            httponly=True,
+            secure=True,
+            samesite='Lax',
+        )
+        
+        response.data = {'message': 'Login successful'}
+        return response
+
+
+class CookieTokenRefreshView(TokenRefreshView):
+    def post(self, request, *args, **kwargs):
+        refresh_token = request.COOKIES.get("refresh_token")
+    
+        if refresh_token is None:
+            return Response({"error": "Refresh token not provided."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        serializer = self.get_serializer(data={"refresh": refresh_token})
+        try:
+            serializer.is_valid(raise_exception=True)
+        except:
+            return Response({"error": "Refresh token is invalid"}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        access_token = serializer.validated_data.get("access")
+
+        response = Response({"message": "Token refreshed successfully"}, status=status.HTTP_200_OK)
+        response.set_cookie(
+            key='access_token',
+            value=access_token,
+            httponly=True,
+            secure=True,
+            samesite='Lax',
+        )
+
+        return response
