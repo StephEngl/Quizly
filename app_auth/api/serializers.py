70195cd0d95b3054f0a7807142b3366eaf4d11c8
Stephanie Englberger <stephaniekoebich@aol.com>
2026-01-09 17:38:48 +0100

    Adapt dealing with user model

--- app_auth/api/serializers.py
+++ app_auth/api/serializers.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class RegistrationSerializer(serializers.ModelSerializer):
     confirmed_password = serializers.CharField(write_only=True)
