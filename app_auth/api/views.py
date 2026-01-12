19188500a7c6f6201eab418b376b407afa867f42
Stephanie Englberger <stephaniekoebich@aol.com>
2026-01-12 12:07:49 +0100

    Clean up code

--- app_auth/api/views.py
+++ app_auth/api/views.py
@@ -2,8 +2,8 @@ from drf_spectacular.utils import extend_schema, OpenApiResponse
 from rest_framework import status
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.views import APIView
 
 from .serializers import RegistrationSerializer, CustomTokenObtainPairSerializer
 
